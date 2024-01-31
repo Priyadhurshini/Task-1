df1e1fb057ce99848cc5e8c586eec3ff73b8ce0a
Priyadhurshini <146009269+Priyadhurshini@users.noreply.github.com>
2024-01-31 18:33:51 +0530

    Create BreakdownContext.js

---
+++ src/Components/BreakdownContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useContext, useState } from 'react';
+
+export const BreakdownContext = createContext();
+
+export const BreakdownProvider = ({ children }) => {
+  const [organizationValue, setOrganizationValue] = useState('');
+
+  const setOrganization = (value) => {
+    setOrganizationValue(value);
+  };
+
+  return (
+    <BreakdownContext.Provider value={{ organizationValue, setOrganization }}>
+      {children}
+    </BreakdownContext.Provider>
+  );
+};
+
+export const useBreakdownContext = () => {
+  return useContext(BreakdownContext);
+};
